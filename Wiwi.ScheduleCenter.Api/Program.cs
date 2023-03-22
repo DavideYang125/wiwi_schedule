1735eefde966c99f5c6ebaa6f1a818e16a4e8889
yww <dayang@it.com>
2023-03-22 14:18:06 +0800

    docker

--- Wiwi.ScheduleCenter.Api/Program.cs
+++ Wiwi.ScheduleCenter.Api/Program.cs
@@ -48,59 +48,24 @@ builder.Host.UseDefaultServiceProvider(options =>
 var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 builder.Services.AddDbContext<ScheduleDbContext>(options =>
 {
-    var connection = Appsettings.Configuration["ConnectionStrings:DefaultConnectionString"];//Configuration.GetConnectionString("DefaultConnectionString");
+    var connection = Appsettings.Configuration["DbConfig:ConnectionString"];
     options.UseMySql(connection, ServerVersion.AutoDetect(connection),
         sql => sql.MigrationsAssembly(migrationAssembly));
 });
 var workId = Appsettings.GetConfig<long>("workId");
 builder.Services.AddSnowFlakeNoGenter(workId);
-//Add CAP
-//builder.Services.AddCapRabbitMq(options =>
-//{
-//    options.UseMySql(mysqlOption =>
-//    {
-//        mysqlOption.ConnectionString = Appsettings.Configuration["DbConfig:ConnectionString"];
-//        mysqlOption.TableNamePrefix = Appsettings.Configuration["CAP:TableNamePrefix"];
-//    });
-//    options.DefaultGroupName = Appsettings.Configuration["CAP:DefaultGroup"];
-//},
-//    mqOptions =>
-//    {
-//        mqOptions.HostName = Appsettings.Configuration["CAP:RabbitMQ:HostName"];
-//        mqOptions.UserName = Appsettings.Configuration["CAP:RabbitMQ:UserName"];
-//        mqOptions.Password = Appsettings.Configuration["CAP:RabbitMQ:Password"];
-//        mqOptions.VirtualHost = Appsettings.Configuration["CAP:RabbitMQ:VirtualHost"];
-//    });
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddHost();
-//builder.Services.AddJobs();
 
-//builder.Services.AddMediatR(typeof(BaseCommand).Assembly);
-
-// NLog：注册Nlog组件
-//builder.Logging.ClearProviders();
 builder.Host.UseNLog();
 
 var app = builder.Build();
 
 
 app.Services.SetServiceProvider();
-//app.Services.AddSnowFlakeNoGenter("1");
-//app.MapGrpcService<GrpcMessageService>();
-//app.MapGet("/GrpcMessage", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-//CodeFirst
-//app.UseCodeFirst();
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.MapGrpcService<GrpcGatewayService>();
-//    app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-//}
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
 app.UseSwaggerMiddleware();
 
 
